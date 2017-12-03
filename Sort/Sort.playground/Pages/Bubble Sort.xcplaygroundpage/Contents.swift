6c29d3df87871fd8be4aa13d32ef4c703c50a979
ADDICE <addice.ch@gmail.com>
2017-12-03 22:56:04 +0800

    完善 冒泡排序 的算法

--- Sort/Sort.playground/Pages/Bubble Sort.xcplaygroundpage/Contents.swift
+++ Sort/Sort.playground/Pages/Bubble Sort.xcplaygroundpage/Contents.swift
@@ -7,18 +7,16 @@ var str = "Hello, playground"
 var sourceArr = [2,3,3,1,9,10,4,6]
 // MARK: Bubble Sort
 func bubbleSort(_ nums: inout [Int]) {
+    // 个数小于 1 的数组，没有比较的必要了～～
+    guard nums.count > 1 else { return }
     
+    // 开始遍历
     for i in 0..<nums.count {
-        var j = nums.count - 1
-        while j > i {
+        // 从数组后面开始，往前遍历比较。知道 i 的位置。如果检查到 nums[j-1] > nums[j]，那么交换位置。
+        for j in (i..<nums.count).reversed() where j > i {
             if nums[j - 1] > nums[j] {
-                print("\(nums[j-1])  \(nums[j])")
-                let temp = nums[j - 1]
-                nums[j - 1] = nums[j]
-                nums[j] = temp
+                nums.swapAt(j, j - 1)
             }
-
-            j -= 1
         }
     }
 }
